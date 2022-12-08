dc1f5f7f929c43d9817bb89c20bc344e6801784a
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-12-08 15:45:50 +0900

    2022-12-08-1540

--- ClangC11Console/ClangC11Sample/HellowWorld.c
+++ ClangC11Console/ClangC11Sample/HellowWorld.c
@@ -1,13 +1,13 @@
 //==== Template ====
 
 /**
-*@title ClangCplus2023 / ClangC11Console / ClanC11Sample
-*@class .c
+*@title ClangC11Console / ClanC11Sample / C99YH / C99YH00_
+*@class Main.c
 *@reference C99YH  結城 浩 『C言語プログラミングレッスン [入門編] 第３版』SB Creative, 2019
 *@reference CAnsi  結城 浩 『C言語プログラミングレッスン [文法編] 新版』  SB Creative, 2006
 *@reference C11DS  arton  『独習 C 新版』翔泳社, 2018
 *
-*@content
+*@content C99YH p /
 *@summary
 *
 *@subject
@@ -43,23 +43,20 @@
 *                └ ClangC11Console.obj
 *                └ hello.obj など
 *
-*@subject int   main(void)
-*         void  printf(string)
-*
 *@see
 *@author shika
 *@date 2022-12-06
 */
 
-#include <stdio.h>
+//#include <stdio.h>
 
 //int main(void);
 
-int main(void) {
-	printf("Hello World, Clang.");
-
-	return 0;
-}//main()
+//int main(void) {
+//	printf("Hello World, Clang.");
+//
+//	return 0;
+//}//main()
 
 /*
 //==== Result ====
