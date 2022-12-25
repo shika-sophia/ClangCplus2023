f40ef1dd1328a5afe4b459a17c9c5b2243f1f15e
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-12-25 09:21:34 +0900

    2022-12-25-0920

--- ClangC11Console/ClangC11Sample/C99YH/C99YH09_Array/MainArraySizeofSample.c
+++ ClangC11Console/ClangC11Sample/C99YH/C99YH09_Array/MainArraySizeofSample.c
@@ -8,19 +8,49 @@
 *@content C99YH 第９章 配列 | Chapter 9  Array / List 9-7 / p267
 *@summary MainArraySizeofSample
 *         ・Initialize Array
-*         ・operator 'sizeof'
+*         ・operator 'sizeof()'
 * 
-*@subject
+*@subject Initialize Array
+* 
+*         [Example]
+*         int  pointAry[] = { 65, 90, 72, };
 *
-*@see
+*@subject operator 'sizeof()'
+* 
+* 
+*@see MainArrayAverageSample.c
 *@author shika
 *@date 2022-12-25
 */
 
 #include <stdio.h>
 
-int main(void) {
-//int mainArraySizeofSample(void) {
+//int main(void) {
+int mainArraySizeofSample(void) {
+    int pointAry[] = { 65, 90, 72, };
+    int sum = 0;
+    int length = sizeof(pointAry) / sizeof(pointAry[0]);
+    double average = 0.0;
+
+    printf("Points:  ");
+    for (int i = 0; i < length; i++) {
+        sum += pointAry[i];
+        printf("%d, ", pointAry[i]);
+    }//for
+    printf("\n");
+
+    average = (double)sum / length;
+
+    printf("Summary: %d\n", sum);
+    printf("Average: %.2f\n", average);
 
     return 0;
 }//main()
+
+/*
+//====== Result =======
+Points:  65, 90, 72,
+Summary: 227
+Average: 75.67
+
+*/
