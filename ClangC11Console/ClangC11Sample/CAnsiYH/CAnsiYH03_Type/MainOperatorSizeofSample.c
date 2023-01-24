501b0c7ec1550a76698bbb3e742609429260a9cb
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2023-01-24 15:44:45 +0900

    2023-01-24-1540

--- ClangC11Console/ClangC11Sample/CAnsiYH/CAnsiYH03_Type/MainOperatorSizeofSample.c
+++ ClangC11Console/ClangC11Sample/CAnsiYH/CAnsiYH03_Type/MainOperatorSizeofSample.c
@@ -92,8 +92,8 @@ typedef struct SchoolMate {
     long totalIndividual;
 } MATE;
 
-int main(void) {
-//int mainXxxx(void) {
+//int main(void) {
+int mainOperatorSizeofSample(void) {
     //---- [Example 1] ----
     printf("char : %u \n", sizeof(char));
     printf("char*: %u \n", sizeof(char*));
