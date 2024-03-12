68ea85135030dc2f9e4d535a420195cb3a53e84a
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2024-03-12 11:12:15 +0900

    2024-02-12

--- ClangC11Console/ClangC11Sample/C11DS_02/hello_world_C11DS.c
+++ ClangC11Console/ClangC11Sample/C11DS_02/hello_world_C11DS.c
@@ -20,3 +20,35 @@ int main(int argc, char *argv[])
     }//if,else
     return 0;
 }//main
+
+/*
+【実行環境】
+コマンドプロンプトVS2019
+環境変数にリンカーを登録
+カレントディレクトリに同じリンカーをコピー
+デスクトップにVSからソースファイルをコピー
+
+>cd C:\Users\******\Desktop\C11DS\C11DS_02     //カレントディレクトリに移動
+
+C:\Users\******\Desktop\C11DS\C11DS_02>cl hello_world_C11DS.c  //コンパイル
+Microsoft(R) C/C++ Optimizing Compiler Version 19.29.30147 for x86
+Copyright (C) Microsoft Corporation.  All rights reserved.
+
+hello_world_C11DS.c
+Microsoft (R) Incremental Linker Version 14.29.30147.0
+Copyright (C) Microsoft Corporation.  All rights reserved.
+
+/out:hello_world_C11DS.exe
+hello_world_C11DS.obj
+
+C:\Users\******\Desktop\C11DS\C11DS_02>hello_world_C11DS   //実行
+hello world!
+
+C:\Users\******\Desktop\C11DS\C11DS_02>hello_world_C11DS 1 2 3 4 5
+sum = 15
+
+C:\Users\******\Desktop\C11DS\C11DS_02>hello_world_C11DS hello 2024
+sum = 2024
+
+C:\Users\******\Desktop\C11DS\C11DS_02>exit
+*/
