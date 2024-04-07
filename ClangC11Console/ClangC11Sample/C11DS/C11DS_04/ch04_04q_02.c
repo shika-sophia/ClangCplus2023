60c59fe610ddad93a6fafbace2c2a1fe0e8d2549
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2024-04-07 15:53:06 +0900

    2024-04-07-1600

--- ClangC11Console/ClangC11Sample/C11DS/C11DS_04/ch04_04q_02.c
+++ ClangC11Console/ClangC11Sample/C11DS/C11DS_04/ch04_04q_02.c
@@ -9,20 +9,21 @@
 
 int main(int argc, char* argv[])
 {
-    int x = atoi(argv[1]);
-    
     if (argc != 2) {
         puts("specify a number");
     }
-    else if (x == 123) {
-        puts("Bingo!");
-    }
-    else if (x > 123) {
-        puts("greater");
-    }
-    else if (x < 123) {
-        puts("less");
-    }
+    else {
+        int x = atoi(argv[1]);
+        if (x == 123) {
+            puts("Bingo!");
+        }
+        else if (x > 123) {
+            puts("greater");
+        }
+        else if (x < 123) {
+            puts("less");
+        }
+    }//else
 
     return 0;
 }//main
@@ -38,12 +39,8 @@ Bingo!
 greater
 
 >ch04_04q_02
+specify a number
 
 【考察】コマンドライン引数がない場合が正常に動作していない。
-if (argc == 1) {
-        puts("specify a number");
-        ↓
-if (argc != 2) {
-        puts("specify a number");に変更す。
-=>変更しても同じ症状。解答はこれなのだが・・・
-*/
+       x = atoi(argv[1]); を　if-else 文の中に入れたら解決。
+ */
