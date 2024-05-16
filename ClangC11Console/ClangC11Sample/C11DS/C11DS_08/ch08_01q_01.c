cdbd537e7b22956b1edc83e8d4a5073d69b34947
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2024-05-16 10:18:53 +0900

    2024-05-16-10000

---
+++ ClangC11Console/ClangC11Sample/C11DS/C11DS_08/ch08_01q_01.c
@@ -0,0 +1,14 @@
+/*
+(3) xpを利用してその値 1238 を出力するプログ(3) xpを利用してその値 1238 を出力するプログラム。
+*/
+
+#include <stdio.h>
+#include <stdint.h>
+
+int main(void)
+{
+    int32_t x = 1234;
+    int32_t* xp = &x;
+    *xp += 4;
+    printf("x = %i, xp = %p\n", *xp, xp);
+}//main
